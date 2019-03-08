29f15702e6de2916af25ac4581ebd5c4fce2b981
kimathikaai <kiprotich.kaai@gmail.com>
2019-03-08 14:05:59 -0500

    Lists and controlled components in react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MyComponent from './components/forms/Reservation';
+
+ReactDOM.render(
+    <MyComponent />,
+    document.getElementById('root')
+);
